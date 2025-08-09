e463d067041050d07e43b756c51159702e138f5b
gi613ts <malkowski.m.a@gmail.com>
2025-08-09 18:15:52 +0200

    2.5 Draw Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player
 
 def main():
 
@@ -9,12 +10,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player_ship = player.Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
+
+        player_ship.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
